0a4986a97a17c142ba5906990c100a97d1329daa
zPippo <filippo.munarin.22@gmail.com>
2024-08-01 17:25:43 +0200

    First commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,142 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] lines = new int[27];
+
+        lines[0] =  0b1000000111111111;
+        lines[1] =  0b1000001011111111;
+        lines[2] =  0b1000001100000011;
+        lines[3] =  0b0000000000000000;
+        lines[4] =  0b0000000000000000;
+        lines[5] =  0b0000000000000000;
+        lines[6] =  0b0010000000000000;
+        lines[7] =  0b0011000000000000;
+        lines[8] =  0b0100000000000000;
+        lines[9] =  0b0101000000000000;
+        lines[10] = 0b0110000000000000;
+        lines[11] = 0b0111000000000000;
+        lines[12] = 0b1000000000000000;
+        lines[13] = 0b1001000111111111;
+        lines[14] = 0b1011000000010011;
+        lines[15] = 0b1100000000011010;
+        lines[16] = 0b1110000000000000;
+        lines[17] = 0b1111000000000000;
+        lines[18] = 0b1010000000000100;
+        lines[19] = 0b1001001011111111;
+        lines[20] = 0b1011000000010110;
+        lines[21] = 0b1010000000000100;
+        lines[22] = 0b1001001111111111;
+        lines[23] = 0b1011000000011001;
+        lines[24] = 0b1010000000000011;
+        lines[25] = 0b0001000000000000;
+        lines[26] = 0b1101000000000000;
+
+        int pc = 0;
+        boolean[] flags = {false, false};
+        int[] regs = new int[16];
+        int[] ram = new int[256];
+        ArrayList<Integer> stack = new ArrayList<>(32);
+
+        int cycles = 0;
+
+        boolean running = true;
+
+        int result;
+
+        while(running) {
+            regs[0] = 0;
+
+            int instruction = lines[pc];
+
+            int opcode = instruction >> 12;
+            int regA = instruction >> 8 & 15;
+            int regB = instruction >> 4 & 15;
+            int regDest = instruction & 15;
+            int immediate = instruction & 255;
+            int condition = instruction >> 10 & 3;
+            int address = instruction & 1023;
+            int ram_offset = instruction & 15;
+            ram_offset = (ram_offset & 8) != 0 ? ram_offset - 16 : ram_offset;
+
+            pc += 1;
+            cycles += 1;
+
+            switch (opcode) {
+                case 1:
+                    running = false;
+                    System.out.println("Halted");
+                    break;
+                case 2:
+                    result = regs[regA] + regs[regB];
+                    regs[regDest] = result & 255;
+                    flags[0] = result > 255;
+                    flags[1] = regs[regDest] == 0;
+                    break;
+                case 3:
+                    result = regs[regA] - regs[regB];
+                    regs[regDest] = result & 255;
+                    flags[0] = regs[regA] >= regs[regB];
+                    flags[1] = regs[regDest] == 0;
+                    break;
+                case 4:
+                    regs[regDest] = 0b11111111 ^ (regs[regA] | regs[regB]);
+                    flags[1] = regs[regDest] == 0;
+                    break;
+                case 5:
+                    regs[regDest] = regs[regA] & regs[regB];
+                    flags[1] = regs[regDest] == 0;
+                    break;
+                case 6:
+                    regs[regDest] = regs[regA] ^ regs[regB];
+                    flags[1] = regs[regDest] == 0;
+                    break;
+                case 7:
+                    regs[regDest] = regs[regA] >> 1;
+                    break;
+                case 8:
+                    regs[regA] = immediate;
+                    break;
+                case 9:
+                    result = regs[regA] + immediate;
+                    regs[regA] = result & 255;
+                    // System.out.print(regA == 3 ? String.valueOf(regs[regA]) + "\n" : "");
+                    flags[0] = result > 255;
+                    flags[1] = regs[regA] == 0;
+                    break;
+                case 10:
+                    pc = address;
+                    break;
+                case 11:
+                    switch (condition) {
+                        case 0 -> pc = flags[1] ? address : pc;
+                        case 1 -> pc = !flags[1] ? address : pc;
+                        case 2 -> pc = flags[0] ? address : pc;
+                        case 3 -> pc = !flags[0] ? address : pc;
+                    }
+                    break;
+                case 12:
+                    stack.add(0, pc);
+                    break;
+                case 13:
+                    pc = stack.remove(0);
+                    break;
+                case 14:
+                    regs[regB] = ram[regA + ram_offset];
+                    break;
+                case 15:
+                    ram[regA + ram_offset] = regs[regB];
+                    break;
+            }
+
+        }
+
+        System.out.println(String.valueOf(cycles) + " cycles executed");
+
+    }
+
+}
